063b4e13284744b306b6285224b0b8dae8f93a66
Cleberson Pertile <c.pertile@icloud.com>
2018-11-04 16:10:59 -0200

    implementada função que verifica se o jogador quer desistir por ter perdido a peça mais significativa

--- Dablot.c
+++ Dablot.c
@@ -32,6 +32,7 @@
 bool validar_alcance(const int[*], const char[TAM_X_TAB][TAM_Y_TAB], pontoCardeal*, pontoCardeal*);
 bool validar_destino(const int[*], const int[*], char[TAM_X_TAB][TAM_Y_TAB], pontoCardeal, pontoCardeal, char[*], char[*]);
 bool validar_peca(const int*, const int[*], const char[TAM_X_TAB][TAM_Y_TAB]);
+bool verificar_desistencia(int, char[*], char[*], size_t, char[*], char[*], size_t);
 bool verificar_pecas_livres(char[TAM_X_TAB][TAM_Y_TAB], const int*, int[*], pontoCardeal*, pontoCardeal*);
 char menu_inicial(void);
 char menu_pausa(void);
@@ -109,6 +110,11 @@ int main(void) {
                     game = false; break;
                 }
 
+                if (verificar_desistencia(jog_atual, nome_jogador1, nome_jogador2, TAM_NOME, pecas_jog1, pecas_jog2, TAM_VETOR)) {
+                    puts("Por desistência, jogo encerrado...");
+                    game = false; break;
+                }
+
                 if (!pedir_entrada(&jog_atual, peca, nome_jogador1, nome_jogador2)) {
                     switch(menu_pausa()) {
                         case 'V': continue;
@@ -143,6 +149,44 @@ int main(void) {
         sleep(3);
     }
     return EXIT_SUCCESS;
+}
+
+bool verificar_desistencia(int jog_atual, char nome_jogador1[], char nome_jogador2[], size_t tam_nome,
+    char pecas_jog1[], char pecas_jog2[], size_t tam_vetor_pecas) {
+    // Verificar nos vetores peças se o jogador perdeu sua peça mais significativa ('R' ou 'F') e perguntar se deseja desistir do jogo
+    // Se for jogador 1, vai olhar para o vetor de peças 1 na posição do 'R'
+    // Jogador 2, olha o vetor de peças 2 na posição 'F'
+    char escolha = '?';
+
+    if (jog_atual == 1) {
+        if (pecas_jog1[tam_vetor_pecas-1] != 'R') {
+            while ((escolha != 'S') && (escolha != 'N')) {
+                printf(VRML "%s" CNZA ", o Rei foi eliminado... Deseja desistir do jogo? [ (S) - (N) ]", nome_jogador1);
+                scanf(" %c", &escolha);
+                escolha = toupper(escolha);
+                switch (escolha) {
+                    case 'S': return true;
+                    case 'N': return false;
+                    default: continue;
+                }
+            }
+        }
+    } else {
+        if (pecas_jog2[tam_vetor_pecas-1] != 'F') {
+            while ((escolha != 'S') && (escolha != 'N')) {
+                printf(VERD "%s" CNZA ", o Fazendeiro foi eliminado... Deseja desistir do jogo? [ (S) - (N) ]", nome_jogador2);
+                scanf(" %c", &escolha);
+                escolha = toupper(escolha);
+                switch (escolha) {
+                    case 'S': return true;
+                    case 'N': return false;
+                    default: continue;
+                }
+            }
+        }
+    }
+    return false;
+
 }
 
 bool verificar_pecas_livres(char matriz_posicao[TAM_X_TAB][TAM_Y_TAB], const int *jog_atual, int peca[], pontoCardeal *movimento, pontoCardeal *captura) {
